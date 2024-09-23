8849a9b3c6835ed0fc37ad093706db58fb0603d0
JiinHong <com5942@seoultech.ac.kr>
2024-09-23 15:58:31 +0900

    init

---
+++ src/main/java/com/example/goorm_ticket/GoormTicketApplication.java
@@ -0,0 +1,13 @@
+package com.example.goorm_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoormTicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/goorm_ticket/GoormTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goorm_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
